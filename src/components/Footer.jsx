78d29ad4701c4e78eee04e6be2fad2a787633cca
unknown <glennntoutoume8@gmail.com>
2025-10-17 16:01:43 +0100

    Implementation de la sectin Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { footerLinks } from "../../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="info">
+        <p>More ways to shop: Find Apple Store or other retailer near you.</p>
+        <img src="/logo.svg" alt="Apple Logo" />
 
-export default Footer
+        <hr />
+        <div className="links">
+          <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+          <ul>
+            {footerLinks.map(({ label, link }) => (
+              <li key={label}>
+                <a href={link}>{label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
